13901db2830f02d9d2f27ce88a39f34ca1f361a0
Jean-Michel Dusserre <jedusser@z3r12p3.42lyon.fr>
2025-05-21 15:35:11 +0200

    start of cpp09

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jedusser <jedusser@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/21 14:50:56 by jedusser          #+#    #+#             */
+/*   Updated: 2025/05/21 14:55:22 by jedusser         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOIN_EXCHANGE_HPP
+#define BITCOIN_EXCHANGE_HPP
+
+class Date
+{
+    private :
+    
+        int _year;
+        int _month;
+        int _day;
+};
+
+class BitcoinExchange
+{
+    
+};
+
+#endif
---
+++ ex00/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jedusser <jedusser@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/21 14:50:59 by jedusser          #+#    #+#             */
+/*   Updated: 2025/05/21 15:19:40 by jedusser         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <BitcoinExchange.hpp>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+    {
+        std::cout << "Usage: <./first_ex> <input file>" << std::endl;
+        return (1);
+    }
+  
+    
+    return (0);
+}
